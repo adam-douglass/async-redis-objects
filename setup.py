aed6da9e7c560cc91c9c1a92e49906e22609e257
Adam Douglass <douglass@malloc.ca>
2020-01-04 17:41:40 -0500

    Add loading readme to description to setup

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import os.path
 from setuptools import setup, find_packages
 
 setup(
@@ -9,11 +10,13 @@ setup(
     author="Adam Douglass",
     author_email="douglass@malloc.ca",
     description="Object orient interface to aioredis.",
+    long_description=open(os.path.join(os.path.dirname(__file__), 'readme.md')).read(),
+    long_description_content_type='text/markdown',
     keywords="utility redis oop object-oriented",
     url="https://github.com/adam-douglass/async-redis-objects/",
 
     extras_require={
         'test': ['pytest', 'pytest-asyncio']
     },
-    install_requires=['aioredis']
+    install_requires=['aioredis'],
 )
